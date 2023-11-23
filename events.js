b2e45a871b85e4b14e7fccd31a8ef64ce4103269
Albina Yermek <yermekalbina@gmail.com>
2023-11-24 03:51:31 +0600

    upd

--- events.js
+++ events.js
@@ -1,24 +1,20 @@
-
 var countDownDate = new Date("Dec 3, 2023 11:00").getTime();
 
 var x = setInterval(function() {
+    var now = new Date().getTime();
 
+    var distance = countDownDate - now;
 
-  var now = new Date().getTime();
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  var distance = countDownDate - now;
+    document.getElementById("countdown-container").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("countdown-container").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown-container").innerHTML = "EXPIRED";
-  }
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown-container").innerHTML = "EXPIRED";
+    }
 }, 1000);
